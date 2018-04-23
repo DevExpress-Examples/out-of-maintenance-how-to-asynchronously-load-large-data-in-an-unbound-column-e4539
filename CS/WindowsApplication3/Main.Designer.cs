23991b38aac704b2dc420e999a19114a988c6b95
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsApplication3/Main.Designer.cs
@@ -0,0 +1,162 @@
+﻿// Developer Express Code Central Example:
+// How to load data in an unbound column asynchronously
+// 
+// This example illustrates how to load data in an unbound column asynchronously.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E3140
+
+namespace DXSample {
+    partial class Main {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.ordersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nwindDataSet = new DXSample.nwindDataSet();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colOrderID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colCustomerID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colEmployeeID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colFreight = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colShipName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.ordersTableAdapter = new DXSample.nwindDataSetTableAdapters.OrdersTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Money Twins";
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.ordersBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(910, 290);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // ordersBindingSource
+            // 
+            this.ordersBindingSource.DataMember = "Orders";
+            this.ordersBindingSource.DataSource = this.nwindDataSet;
+            // 
+            // nwindDataSet
+            // 
+            this.nwindDataSet.DataSetName = "nwindDataSet";
+            this.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colOrderID,
+            this.colCustomerID,
+            this.colEmployeeID,
+            this.colFreight,
+            this.colShipName});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.RowAutoHeight = true;
+            // 
+            // colOrderID
+            // 
+            this.colOrderID.FieldName = "OrderID";
+            this.colOrderID.Name = "colOrderID";
+            this.colOrderID.Visible = true;
+            this.colOrderID.VisibleIndex = 0;
+            // 
+            // colCustomerID
+            // 
+            this.colCustomerID.FieldName = "CustomerID";
+            this.colCustomerID.Name = "colCustomerID";
+            this.colCustomerID.Visible = true;
+            this.colCustomerID.VisibleIndex = 1;
+            // 
+            // colEmployeeID
+            // 
+            this.colEmployeeID.FieldName = "EmployeeID";
+            this.colEmployeeID.Name = "colEmployeeID";
+            this.colEmployeeID.Visible = true;
+            this.colEmployeeID.VisibleIndex = 2;
+            // 
+            // colFreight
+            // 
+            this.colFreight.FieldName = "Freight";
+            this.colFreight.Name = "colFreight";
+            this.colFreight.Visible = true;
+            this.colFreight.VisibleIndex = 3;
+            // 
+            // colShipName
+            // 
+            this.colShipName.FieldName = "ShipName";
+            this.colShipName.Name = "colShipName";
+            this.colShipName.Visible = true;
+            this.colShipName.VisibleIndex = 4;
+            // 
+            // ordersTableAdapter
+            // 
+            this.ordersTableAdapter.ClearBeforeFill = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 290);
+            this.Controls.Add(this.gridControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Main";
+            this.Text = "AsyncLoading";
+            this.Load += new System.EventHandler(this.OnFormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private nwindDataSet nwindDataSet;
+        private System.Windows.Forms.BindingSource ordersBindingSource;
+        private DXSample.nwindDataSetTableAdapters.OrdersTableAdapter ordersTableAdapter;
+        private DevExpress.XtraGrid.Columns.GridColumn colOrderID;
+        private DevExpress.XtraGrid.Columns.GridColumn colCustomerID;
+        private DevExpress.XtraGrid.Columns.GridColumn colEmployeeID;
+        private DevExpress.XtraGrid.Columns.GridColumn colFreight;
+        private DevExpress.XtraGrid.Columns.GridColumn colShipName;
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to asynchronously load large data in an unbound column
+
+
+<p>This example demonstrates how to asynchronously load large data in an unbound column.</p><p>See also: <a href="https://www.devexpress.com/Support/Center/p/E3140">How to load data in an unbound column asynchronously</a></p>
+
+<br/>
+
+
